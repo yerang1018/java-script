9feaafe739fc106fb13788354ab31fe5e8d677ea
yerang1018 <145122325+yerang1018@users.noreply.github.com>
2024-02-26 23:34:41 +0900

    filter 를 이용한 데이터 추출
    form 에 있는 input 의 여러가지 type 을 이용하여
    검색필터, 다중 조건 필터를 구현해보았다.

---
+++ day06/homeplus(berry).js
@@ -0,0 +1,802 @@
+const arr = [
+    {
+        "name": "새벽 겨울 딸기 500G(팩)",
+        "price": 9990,
+        "salePrice": 9990,
+        "salesCount": 181665,
+        "grade": 4.6,
+        "reviewCount": 27902
+    },
+    {
+        "name": "슈퍼푸드 블루베리(칠레) 310G(팩)",
+        "price": 9990,
+        "salePrice": 9990,
+        "salesCount": 43939,
+        "grade": 4.6,
+        "reviewCount": 4144
+    },
+    {
+        "name": "알큰딸기 700G(박스)",
+        "price": 14990,
+        "salePrice": 14990,
+        "salesCount": 42568,
+        "grade": 4.5,
+        "reviewCount": 2824
+    },
+    {
+        "name": "달콤한 체리(칠레) 500G(팩)",
+        "price": 14990,
+        "salePrice": 14990,
+        "salesCount": 14292,
+        "grade": 4,
+        "reviewCount": 205
+    },
+    {
+        "name": "금실딸기 500G(팩)",
+        "price": 17990,
+        "salePrice": 17990,
+        "salesCount": 3482,
+        "grade": 4.5,
+        "reviewCount": 133
+    },
+    {
+        "name": "살살녹는 장희 딸기 750G(팩)",
+        "price": 22990,
+        "salePrice": 22990,
+        "salesCount": 1838,
+        "grade": 4.1,
+        "reviewCount": 70
+    },
+    {
+        "name": "겨울딸기 생딸기 산지직송 못난이 1kg",
+        "price": 14500,
+        "salePrice": 14500,
+        "salesCount": 5,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "산지직송 서산 설향 딸기 1kg(30-35과내외)",
+        "price": 42900,
+        "salePrice": 42900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "산지직송 서산 금실 딸기 1kg(30-35과내외)",
+        "price": 54900,
+        "salePrice": 54900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "산지직송 서산 킹스베리 딸기 1kg(22-26과내외)",
+        "price": 54900,
+        "salePrice": 54900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 칠레산 체리 500g",
+        "price": 20800,
+        "salePrice": 20800,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 칠레산 체리 1kg (500g x 2팩)",
+        "price": 35400,
+        "salePrice": 35400,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] GAP인증 국내산 논산 설향 딸기 800g",
+        "price": 26700,
+        "salePrice": 26700,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] GAP인증 국내산 논산 비타베리 딸기 800g",
+        "price": 32600,
+        "salePrice": 32600,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 국산 냉동과일 냉동 딸기 3kg",
+        "price": 36300,
+        "salePrice": 36300,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 중국산 냉동과일 냉동 딸기 3kg",
+        "price": 28300,
+        "salePrice": 28300,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 칠레산 맛있는 생 블루베리 125g x 3팩",
+        "price": 22700,
+        "salePrice": 22700,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 칠레산 맛있는 생 블루베리 125g x 6팩",
+        "price": 40000,
+        "salePrice": 40000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 칠레산 맛있는 생 블루베리 125g x 12팩",
+        "price": 73700,
+        "salePrice": 73700,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 캐나다산 맛있는 간편 냉동 블루베리 3kg",
+        "price": 43000,
+        "salePrice": 43000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[프레쉬푸드] 페루산 맛있는 간편 냉동 블루베리 3kg",
+        "price": 36900,
+        "salePrice": 36900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[세계3대돈육] 뼈는 줄이고 살로 가득채운 듀록 돈마호크 1대",
+        "price": 10900,
+        "salePrice": 10900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "칠레산 신선한 블루베리 생과 125g x2팩",
+        "price": 10900,
+        "salePrice": 10900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "칠레산 신선한 블루베리 생과 125g x4팩",
+        "price": 15900,
+        "salePrice": 15900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "칠레산 신선한 블루베리 생과 125g x6팩",
+        "price": 20900,
+        "salePrice": 20900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "칠레산 신선한 블루베리 생과 125g x8팩",
+        "price": 25900,
+        "salePrice": 25900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "칠레산 신선한 블루베리 생과 125g x10팩",
+        "price": 29900,
+        "salePrice": 29900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "칠레산 신선한 블루베리 생과 125g x12팩",
+        "price": 34900,
+        "salePrice": 34900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "탱글함이 살아 있는 체리 4kg",
+        "price": 89900,
+        "salePrice": 89900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "탱글함이 살아 있는 체리 2kg",
+        "price": 46900,
+        "salePrice": 46900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "(민수팜) 새콤달콤 신선한 딸기 2kg 특",
+        "price": 56900,
+        "salePrice": 56900,
+        "salesCount": 3,
+        "grade": 5,
+        "reviewCount": 1
+    },
+    {
+        "name": "(민수팜)새콤달콤 신선 딸기 2kg 상",
+        "price": 51900,
+        "salePrice": 51900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "겨울딸기 생딸기 데코용 300g x 2상자",
+        "price": 25900,
+        "salePrice": 25900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[후레쉬데이] 칠레산 생 체리 1kg (10R 사이즈)",
+        "price": 38900,
+        "salePrice": 38900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[후레쉬데이] 칠레산 생 체리 500g (10R 사이즈)",
+        "price": 24900,
+        "salePrice": 24900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[후레쉬데이] 칠레산 생 체리 2kg (10R 사이즈)",
+        "price": 67900,
+        "salePrice": 67900,
+        "salesCount": 2,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "★(과일먹는곰) 논산 킹스베리 1kg",
+        "price": 42500,
+        "salePrice": 42500,
+        "salesCount": 2,
+        "grade": 5,
+        "reviewCount": 1
+    },
+    {
+        "name": "칠레산 생체리 500g 최상급 프리미엄 고당도 품질보증",
+        "price": 19900,
+        "salePrice": 19900,
+        "salesCount": 3,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "뿌리까지갈아낸 생도라지 하루한포 도라지차 액상스틱 600g",
+        "price": 45000,
+        "salePrice": 45000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "뿌리까지 갈아낸 제주더덕 하루한포 더덕차 액상스틱 600g",
+        "price": 45000,
+        "salePrice": 45000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "제주가 키운 과일차 3종 선물세트 1호 과일청 답례품 750g",
+        "price": 20000,
+        "salePrice": 20000,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "제주가 키운 과일차&야생꿀 선물세트 2호 꿀차 750g",
+        "price": 20000,
+        "salePrice": 20000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "제주가 키운 과일차 2종 선물세트 5호 (한라봉차/오미자차)",
+        "price": 30000,
+        "salePrice": 30000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "제주가 키운 과일차 2종 선물세트 13호 (한라봉차/풋귤차)",
+        "price": 12000,
+        "salePrice": 12000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 500g (10row)",
+        "price": 14900,
+        "salePrice": 14900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 1kg (10row)",
+        "price": 23900,
+        "salePrice": 23900,
+        "salesCount": 2,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 1.5kg (10row)",
+        "price": 31900,
+        "salePrice": 31900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 2kg (10row)",
+        "price": 39900,
+        "salePrice": 39900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 3kg (10row)",
+        "price": 57900,
+        "salePrice": 57900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 5kg (10row)",
+        "price": 89900,
+        "salePrice": 89900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 500g 큰사이즈(9.5row)",
+        "price": 15900,
+        "salePrice": 15900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 1kg 큰사이즈(9.5row)",
+        "price": 24900,
+        "salePrice": 24900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 1.5kg 큰사이즈(9.5row)",
+        "price": 33900,
+        "salePrice": 33900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 2kg 큰사이즈(9.5row)",
+        "price": 42900,
+        "salePrice": 42900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 3kg 큰사이즈(9.5row)",
+        "price": 61900,
+        "salePrice": 61900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "프리미엄 칠레산 고당도 생체리 5kg 큰사이즈(9.5row)",
+        "price": 97900,
+        "salePrice": 97900,
+        "salesCount": 2,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "생체리 두근 800g 로얄 9~10R 겨울체리",
+        "price": 32000,
+        "salePrice": 32000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "생체리 두근두근 1.6kg 로얄 9~10R 겨울체리",
+        "price": 55000,
+        "salePrice": 55000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "겨울 장희 생딸기 특품 750g",
+        "price": 25900,
+        "salePrice": 25900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "겨울 장희 생딸기 특품 1.5kg",
+        "price": 44900,
+        "salePrice": 44900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[산지직송]첫출하 논산 딸기(설향) 500g 특품",
+        "price": 22900,
+        "salePrice": 22900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[산지직송]첫출하 논산 딸기(설향) 500g x2(1kg) 특품",
+        "price": 39900,
+        "salePrice": 39900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[산지직송]첫출하 논산 딸기(설향) 700g 특품",
+        "price": 27900,
+        "salePrice": 27900,
+        "salesCount": 3,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[산지직송]첫출하 논산 딸기(설향) 700g x2(1.4kg) 특품",
+        "price": 49900,
+        "salePrice": 49900,
+        "salesCount": 3,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "산청 알찬딸기 실속 1kg 랜덤 새콤달콤 맛좋고 향좋은",
+        "price": 43000,
+        "salePrice": 43000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "산청 알찬딸기 상품 1kg 55과내외 새콤달콤 맛좋고 향좋은",
+        "price": 46000,
+        "salePrice": 46000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "산청 알찬딸기 로얄 1kg 40과내외 새콤달콤 맛좋고 향좋은",
+        "price": 53000,
+        "salePrice": 53000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "호재준 냉동 유기농 블루베리 500g x 3팩",
+        "price": 28800,
+        "salePrice": 28800,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "호재준 냉동 유기농 블루베리 500g x 1팩",
+        "price": 9900,
+        "salePrice": 9900,
+        "salesCount": 4,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[논산] 쥬스용 냉동딸기 2kg (500gX4봉)",
+        "price": 22500,
+        "salePrice": 22500,
+        "salesCount": 9,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "급냉 산딸기 1kgx4팩",
+        "price": 70900,
+        "salePrice": 70900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "급냉 산딸기 1kgx3팩",
+        "price": 54900,
+        "salePrice": 54900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "급냉 산딸기 1kgx2팩",
+        "price": 37900,
+        "salePrice": 37900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "급냉 산딸기 1kgx1팩",
+        "price": 21500,
+        "salePrice": 21500,
+        "salesCount": 8,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[산지직송] 경남 산청 달콤한 딸기 1kg (중품)",
+        "price": 18800,
+        "salePrice": 18800,
+        "salesCount": 190,
+        "grade": 5,
+        "reviewCount": 2
+    },
+    {
+        "name": "별별딸기청550GX2병",
+        "price": 20000,
+        "salePrice": 20000,
+        "salesCount": 2,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "짜먹는딸기잼200GX2개",
+        "price": 10000,
+        "salePrice": 10000,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "이장님냉동딸기500GX4봉",
+        "price": 22500,
+        "salePrice": 22500,
+        "salesCount": 7,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "(민수팜) 새콤달콤 과즙팡팡  설향 딸기 1kg (특)",
+        "price": 32900,
+        "salePrice": 32900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "(민수팜) 새콤달콤  과즙팡팡  설향 딸기 1kg (상)",
+        "price": 29900,
+        "salePrice": 29900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[논산직송] 설향 하우스 생딸기 특품(30구/900g내외)",
+        "price": 27900,
+        "salePrice": 27900,
+        "salesCount": 20,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[논산직송] 설향 하우스 생딸기 상품(35구/700g내외)",
+        "price": 22900,
+        "salePrice": 22900,
+        "salesCount": 34,
+        "grade": 1,
+        "reviewCount": 1
+    },
+    {
+        "name": "칠레산 생체리 1kg 최상급 수입과일 품질보증 맛보증",
+        "price": 33900,
+        "salePrice": 33900,
+        "salesCount": 26,
+        "grade": 5,
+        "reviewCount": 1
+    },
+    {
+        "name": "[산지직송] 경남 산청 달콤한 딸기 1kg (특품)",
+        "price": 23800,
+        "salePrice": 23800,
+        "salesCount": 540,
+        "grade": 4.6,
+        "reviewCount": 10
+    },
+    {
+        "name": "[산지직송] 경남 산청 달콤한 딸기 1kg (상품)",
+        "price": 21800,
+        "salePrice": 21800,
+        "salesCount": 390,
+        "grade": 1,
+        "reviewCount": 1
+    },
+    {
+        "name": "산지직송 유기농 냉동 블루베리 1kg 특품(15mm이상)",
+        "price": 23900,
+        "salePrice": 23900,
+        "salesCount": 26,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "베리필드 블루베리1kg(1kgx1ea)/냉동과일 미국산",
+        "price": 10900,
+        "salePrice": 10900,
+        "salesCount": 107,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동 블루베리 1kg(최상품)",
+        "price": 30900,
+        "salePrice": 30900,
+        "salesCount": 19,
+        "grade": 5,
+        "reviewCount": 1
+    },
+    {
+        "name": "냉동 블루베리 2kg(최상품)",
+        "price": 56900,
+        "salePrice": 56900,
+        "salesCount": 6,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[자연원] 허니딸기 500g x 3팩",
+        "price": 14790,
+        "salePrice": 14790,
+        "salesCount": 7,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[자연원] 허니딸기 500g x 2팩",
+        "price": 10410,
+        "salePrice": 10410,
+        "salesCount": 7,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "[자연원] 허니딸기 500g x 1팩",
+        "price": 5480,
+        "salePrice": 5480,
+        "salesCount": 3,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 500g x 2봉",
+        "price": 16900,
+        "salePrice": 16900,
+        "salesCount": 19,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 500g x 4봉",
+        "price": 24500,
+        "salePrice": 24500,
+        "salesCount": 7,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 500g x 5봉",
+        "price": 28900,
+        "salePrice": 28900,
+        "salesCount": 2,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 500g x 6봉",
+        "price": 31900,
+        "salePrice": 31900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 500g x 8봉",
+        "price": 41500,
+        "salePrice": 41500,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 500g x 10봉",
+        "price": 48500,
+        "salePrice": 48500,
+        "salesCount": 2,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 5kg-벌크",
+        "price": 46900,
+        "salePrice": 46900,
+        "salesCount": 1,
+        "grade": 0,
+        "reviewCount": 0
+    },
+    {
+        "name": "냉동딸기 10kg-벌크",
+        "price": 85900,
+        "salePrice": 85900,
+        "salesCount": 0,
+        "grade": 0,
+        "reviewCount": 0
+    }
+]
